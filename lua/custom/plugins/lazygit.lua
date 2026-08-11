2b0dad87989a1ced790ba8c13ca3aa381057f9d0
kahnix <kacperdev@gmail.com>
2026-08-11 15:04:47 +0200

    fix lazygit crashing when used with mini.starter

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -13,8 +13,14 @@ return {
   keys = {
     {
       '<leader>lg',
-      '<cmd>LazyGitCurrentFile<cr>',
-      desc = 'Open [L]azy[G]it for current file',
+      function()
+        if vim.api.nvim_buf_get_name(0) == '' or vim.bo.buftype ~= '' then
+          vim.cmd 'LazyGit'
+        else
+          vim.cmd 'LazyGitCurrentFile'
+        end
+      end,
+      desc = 'Open [L]azy[G]it',
     },
   },
 }
