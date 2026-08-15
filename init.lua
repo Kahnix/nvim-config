98ae67a25283b93061415b08ac32b3819ec05546
kahnix <kacperdev@gmail.com>
2026-08-15 14:53:23 +0200

    clean up config, add oxlint, oxfmt as main formatters

--- init.lua
+++ init.lua
@@ -96,11 +96,8 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Save bind
+vim.keymap.set('n', '<C-s>', ':w<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -110,12 +107,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -587,7 +578,6 @@ require('lazy').setup({
         gopls = {},
         -- pyright = {},
         rust_analyzer = {},
-        eslint = {},
         vtsls = {},
         lua_ls = {
           -- cmd = { ... },
@@ -647,6 +637,12 @@ require('lazy').setup({
         capabilities = capabilities,
       })
       vim.lsp.enable 'nixd'
+
+      -- same for oxlint
+      vim.lsp.config('oxlint', {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable 'oxlint'
     end,
   },
 
@@ -682,12 +678,12 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        javascript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescript = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        json = { 'prettier' },
-        jsonc = { 'prettier' },
+        javascript = { 'biome', 'oxfmt', 'prettier', stop_after_first = true },
+        javascriptreact = { 'biome', 'oxfmt', 'prettier', stop_after_first = true },
+        typescript = { 'biome', 'oxfmt', 'prettier', stop_after_first = true },
+        typescriptreact = { 'biome', 'oxfmt', 'prettier', stop_after_first = true },
+        json = { 'biome', 'oxfmt', 'prettier', stop_after_first = true },
+        jsonc = { 'biome', 'oxfmt', 'prettier', stop_after_first = true },
 
         go = { 'goimports', 'gofumpt' },
 
@@ -916,7 +912,7 @@ require('lazy').setup({
           return
         end
 
-        if vim.list_contains(treesitter.get_installed('parsers'), lang) then
+        if vim.list_contains(treesitter.get_installed 'parsers', lang) then
           local ok = pcall(vim.treesitter.start, bufnr, lang)
           if ok and not indent_disabled[filetype] then
             vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
